60c1f5ab94961419344ac0df8b2577a8a4c4e57d
Felipe Mariuba Ornelas <71107165+Felipe-Mariuba-Ornelas@users.noreply.github.com>
2020-09-13 13:11:43 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,22 @@
+package br.urionline.judge;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        int A, B, X;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        A = sc.nextInt();
+   
+        B = sc.nextInt();
+       
+        X = B+A;
+        
+        System.out.println("X = " + X);
+        
+    }
+    
+}
