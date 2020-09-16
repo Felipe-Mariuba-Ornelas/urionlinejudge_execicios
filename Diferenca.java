fdfdafcc5048e1764f5638704ec181644070a6cd
Felipe Mariuba Ornelas <71107165+Felipe-Mariuba-Ornelas@users.noreply.github.com>
2020-09-15 21:46:02 -0300

    Add files via upload

---
+++ Diferenca.java
@@ -0,0 +1,21 @@
+package br.urionline.judge;
+
+import java.util.Scanner;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        int A, B, C, D, DIFERENCA;
+        
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        D = sc.nextInt();
+        
+        DIFERENCA = (A * B) - (C * D);
+        
+        System.out.println("DIFERENCA = "+DIFERENCA);
+    }
+    
+}
